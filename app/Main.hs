49d3e1ab5bd6c2c6382970cafcc7a936eee7f759
王元炜 <wangyw20@mails.tsinghua.edu.cn>
2025-04-28 17:10:05 +0800

    Basic Server Commitment

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+-- {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Happstack.Server
+import Control.Monad (msum)
+
+homepage :: ServerPart Response
+homepage = ok $ toResponse "Welcome to the homepage!"
+
+myApp :: ServerPart Response
+myApp = msum 
+    [ dir "hello" $ ok $ toResponse "Hello from /hello"
+    , homepage
+    ]
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn $ "visit http://localhost:" ++ (show servingPort) ++ " or see Hello at http://localhost:" ++ (show servingPort) ++ "/hello"
+    simpleHTTP (nullConf { port = servingPort }) myApp
+    where servingPort = 8000
