6f124ae2b8c076ba7fde2fa5f0a725892f4de743
Yuanwei Wang <davidwangnoi@163.com>
2025-04-29 20:30:15 +0800

    hspec

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( md2html
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Pandoc
+import qualified Data.Text as T
+import Text.StringConvert 
+
+md2html :: String -> IO String
+md2html md = do
+    res <- runIO $ do
+        doc <- readMarkdown def (T.pack md)
+        writeHtml5String def doc
+    ret <- handleError res
+    return (toString ret)
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import Text.Pandoc
+import Test.Hspec
+import Test.QuickCheck
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Test Convert" $ do
+        it "header" $ do
+            htmlString <- md2html "# h1"
+            htmlString `shouldBe` "<h1>h1</h1>"
